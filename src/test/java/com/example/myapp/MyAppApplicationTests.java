940a5b0c5774f94b62ff699dfceb6fe2e0100441
Ubuntu <ubuntu@ip-172-31-64-147.ec2.internal>
2025-11-21 16:03:53 +0000

    ypdated pom.xml file

--- src/test/java/com/example/myapp/MyAppApplicationTests.java
+++ src/test/java/com/example/myapp/MyAppApplicationTests.java
@@ -1,15 +1,11 @@
 package com.example.myapp;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
-public class MyAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-        // This test will pass if the Spring application context loads successfully
-    }
-
+class MyAppApplicationTests {
+@Test
+void contextLoads() {
+    // This test will simply check if the Spring application context loads successfully
+}
 }
 
